06dc9f8bd6198ebebfa0726608251b8a06793d3e
sarahikitty03 <gaitansarahi7@gmail.com>
2025-03-13 12:44:22 -0600

    Subiendo actualizacion practica 4

---
+++ src/database/authcontext.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { appfirebase } from "./firebaseconfig";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(()  => {
+        const auth = getAuth(appfirebase);
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setIsLoggedIn(!!user);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const logout = async () => {
+        const auth = getAuth(appfirebase);
+        await signOut(auth);
+        setUser(null);
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, isLoggedIn, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+};
